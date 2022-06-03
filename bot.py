d58f36d2cebe74cd2626df6a5659ac1efc9c80c5
1-мд-8 Милованова Евгения <103684357+jmilovanova@users.noreply.github.com>
2022-06-03 03:59:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-TOKEN = "5285276974:AAHKPg_-zZoWdWOHpNB4P5vbcqMNHB9CRkg"
+TOKEN = "5582909922:AAFNM1wXLf2AtYbKLqU2FGtAcDlIc9vRLbs"
 import telebot
 if __name__ == "bot":
     bot = telebot.TeleBot(TOKEN)
